4582d43d636ded722b7e9675821cf6c229af7731
zhaojinjie <zhaojinjie@hcylsoft.cn>
2023-11-05 15:43:26 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,24 @@
+// the Uniswap Default token list lives here
+// export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+export const DEFAULT_TOKEN_LIST_URL = 'https://app.tryroll.com/tokens.json'
+// import okbtokenlist from './tokenlist.json'
+
+// const tokenlist = okbtokenlist+"";
+// 这是token 列表  需要做一个自己的token list
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  // 't2crtokens.eth', // kleros
+  // 'tokens.1inch.eth', // 1inch
+  // 'synths.snx.eth',
+  // 'tokenlist.dharma.eth',
+  // 'defi.cmc.eth',
+  // 'erc20.cmc.eth',
+  // 'stablecoin.cmc.eth',
+  // 'tokenlist.zerion.eth',
+  // 'tokenlist.aave.eth',
+  // 'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  // 'https://app.tryroll.com/tokens.json',
+  // 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  // 'https://defiprime.com/defiprime.tokenlist.json',
+  // 'https://umaproject.org/uma.tokenlist.json'
+]
